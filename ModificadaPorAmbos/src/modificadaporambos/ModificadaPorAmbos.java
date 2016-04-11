93f0c4d30041b5b43eef774f0c5c9cb1069e4498
Usuario <Usuario@DESKTOP-8S08D2L>
2016-04-11 14:23:17 +0200

    Hola mundo

---
+++ ModificadaPorAmbos/src/modificadaporambos/ModificadaPorAmbos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modificadaporambos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ModificadaPorAmbos {
+    public static void main(String[] args) {
+        //Clase que vamos a duplicar
+        System.out.println("Clase que añadiremos un método");
+    }
+    public void calculaSuma(){
+    
+        int x=9;
+        int y=1;
+        int suma = x+y;
+        System.out.println(suma);
+    
+    }
+}
